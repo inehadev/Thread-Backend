2df80c05d47813425ed20adaa121fa8ccf9ac0f1
inehadev <156919946+inehadev@users.noreply.github.com>
2024-04-01 23:52:14 +0530

    solved the error of process environment

--- index.js
+++ index.js
@@ -8,26 +8,18 @@ const PostRoutes = require('./Routes/PostRoutes');
 const cors = require('cors')
 const cloudinary = require('cloudinary').v2;
 const dotenv = require('dotenv');
+const connectDB = require('./server/connectDB');
 dotenv.config();
+
+
+
+
 
 const PORT=process.env.PORT || 5000;
 
-const connectDB = async () => {
-	try {
-		const conn = await mongoose.connect(process.env.MONGO_URI, {
-			// To avoid warnings in the console
-			useNewUrlParser: true,
-			useUnifiedTopology: true,
-		});
-
-		console.log(`MongoDB Connected: ${conn.connection.host}`);
-	} catch (error) {
-		console.error(`Error: ${error.message}`);
-		process.exit(1);
-	}
-}
 
 
+connectDB();
 cloudinary.config({
    cloud_name:process.env.CLOUDINARY_CLOUD_NAME,
    api_key:process.env.CLOUDINARY_API_KEY,
---
+++ server/connectDB.js
@@ -0,0 +1,18 @@
+require('dotenv').config();
+const mongoose = require('mongoose');
+const connectDB = async () => {
+	try {
+		const conn = await mongoose.connect(process.env.MONGO_URI, {
+			// To avoid warnings in the console
+			useNewUrlParser: true,
+			useUnifiedTopology: true,
+		});
+
+		console.log(`MongoDB Connected: ${conn.connection.host}`);
+	} catch (error) {
+		console.error(`Error: ${error.message}`);
+		process.exit(1);
+	}
+}
+
+module.exports=connectDB;
